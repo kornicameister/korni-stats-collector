77e94962003cdc5f30faa055fbd3e4383fce6bd3
Tomasz Trębski <kornicameister@gmail.com>
2017-11-15 23:15:45 +0100

    Update setup.py (#8)

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ setup(
     name='ksc',
     description='korni-stats-collector',
     packages=find_packages(exclude=['tests']),
+    author='kornicameister@gmail.com',
+    license='Apache 2.0',
+    long_description=open('README.md').read(),
     exclude_package_data={
         '': ['.git', '.gitignore', '.vscode']
     }
